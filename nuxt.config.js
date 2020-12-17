689297e465da41cfc725ec6d126be6d665272f8f
Lyman Snow <pmvoken@protonmail.com>
2020-12-17 18:58:08 +0000

    updated: nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -213,7 +213,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
-    analyze: true,
+    // analyze: true,
     postcss: {
       plugins: {
         tailwindcss: join(__dirname, 'tailwind.config.js')
